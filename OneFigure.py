b82c839b0ac02250d3c49cd25e3dc00583b8843a
FrankYuDang <yudangneu@gmail.com>
2018-09-21 21:18:50 +1200

    Second commit changes

--- OneFigure.py
+++ OneFigure.py
@@ -10,6 +10,14 @@ x2 = np.linspace(0.0, 2.0)
 print(x1)
 y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
 y2 = np.cos(2 * np.pi * x2)
+# One figure
+one_fig, one_ax = plt.subplots()
+ln1 = one_ax.plot(x1, y1, '.r', label = 'Sin')
+ln2 = one_ax.plot(x2, y2, '.b', label = 'Cos')
+lns = ln1 + ln2
+labs =[l.get_label() for l in lns]
+one_ax.legend(lns, labs, loc=0)
+plt.show()
 
 # Create canvas & modify the figures
 fig = plt.subplot(2, 1, 1)
